eb1f7caf836918dd4ad1178f921d8cf4cba83b40
ypszn <110453347+youngpriince@users.noreply.github.com>
2024-10-29 00:14:36 +0100

    Delete .next directory

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"node_modules/@nillion/client-wasm/dist/worker.js -> ./index.js\":{\"id\":\"node_modules/@nillion/client-wasm/dist/worker.js -> ./index.js\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_nillion_client-wasm_dist_index_js.js\"]}}"
